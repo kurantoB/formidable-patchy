fdc9c48de07008f96d3ce6cb183d34847eb3a75d
Thomas <thomasjnliu@gmail.com>
2020-06-16 17:51:59 -0400

    Add timer to TopicManager script

---
+++ Assets/Scripts/TopicManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopicManager : MonoBehaviour
+{
+    public GameObject[] topics; 
+
+    public float topicSpawnTime;
+    private float topicTimeLeft;
+    bool timerRunning = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get array of all GameObjects with TopicButton tag to spawn later
+        topics = GameObject.FindGameObjectsWithTag("TopicButton");
+        Debug.Log(topics[0]);
+        topicTimeLeft = topicSpawnTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Timer code
+        if(timerRunning){
+             topicTimeLeft -= Time.deltaTime;
+            if (topicTimeLeft <= 0)
+            {
+                topicTimeLeft = topicSpawnTime;
+                Debug.Log("Spawn Topic");
+            }
+        }
+    }
+}
