c35dad90a8e81594bfbdfb0f7bffac252bc205ab
Thomas <thomasjnliu@gmail.com>
2020-06-17 22:38:39 -0400

    Add topic button movement

---
+++ Assets/Scripts/TopicButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TopicButton : MonoBehaviour
+{
+    private RectTransform  rectTransform;
+    public float yPos = -95;
+    public int speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        yPos += Time.deltaTime * speed;
+        rectTransform.anchoredPosition = new Vector2 (125,yPos);
+
+        if(yPos >= 70){
+            Destroy(gameObject);
+        }
+    }
+}
