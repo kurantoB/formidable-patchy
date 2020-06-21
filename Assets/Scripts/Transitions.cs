598fb531dcf99eeccf48c3d8de67dac7fa85a45e
D L <laomeister93@gmail.com>
2020-06-21 01:07:11 -0400

    Manual

--- Assets/Scripts/Transitions.cs
+++ Assets/Scripts/Transitions.cs
@@ -1,10 +1,40 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Transitions : MonoBehaviour
 {
+    public string[] manualInstructions;
+    public Image[] manualImages;
+    private int manualPage = 0;
+    public Text manualText;
+    public GameObject manualNextButton;
+
+    private void Start()
+    {
+        if (SceneManager.GetActiveScene().name.Equals("Manual"))
+        {
+            manualText.text = manualInstructions[0];
+        }
+    }
+
     public void LoadGameScene()
     {
         SceneManager.LoadScene("kurantoScene");
+    }
+
+    public void NextManualPage()
+    {
+        if (manualPage < manualInstructions.Length - 1)
+        {
+            manualImages[manualPage].gameObject.SetActive(false);
+            manualPage++;
+            manualText.text = manualInstructions[manualPage];
+            manualImages[manualPage].gameObject.SetActive(true);
+            if (manualPage == manualInstructions.Length - 1)
+            {
+                manualNextButton.SetActive(false);
+            }
+        }
     }
 }
